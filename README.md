2555aa2ed0fdc3d15c94c76f21c4a11e8d894aca
Raffaele Decarli <raffaele.decarli@thread.solutions>
2020-12-09 14:41:30 +0100

    prova pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# lazyrecon.github.io
---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function ($, viewport) {
+    $(document).ready(function () {
+
+        var external = new Swiper('.swiper-container-ext', {
+            direction: 'vertical',
+            allowTouchMove: false,
+            scrollbar: {
+                draggable: false,
+            },
+        });
+
+        var internal = new Swiper('.swiper-container-inn', {
+            direction: 'vertical',
+            nested: true,
+            allowTouchMove: false,
+            navigation: {
+                nextEl: '.btn-next',
+                prevEl: '.btn-prev',
+            },
+        });
+
+        $('.ext-next-slide').click(function (e){
+            e.preventDefault();
+            external.slideTo(1)
+        })
+        $('.ext-start-slide').click(function (e){
+            e.preventDefault();
+            external.slideTo(0)
+            internal.slideTo(0)
+        })
+
+        $(document).on('click','.btn-prev.swiper-button-disabled', function (e){
+            e.preventDefault();
+            external.slideTo(0)
+        })
+
+        $(document).on('click','.btn-next.swiper-button-disabled', function (e){
+            e.preventDefault();
+            external.slideTo(2)
+        })
+
+    });
+})(jQuery, ResponsiveBootstrapToolkit);
