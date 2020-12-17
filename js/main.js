3c7e3f453bacdc87ccbde6b613a6a3d28c3a26c3
Raffaele Decarli <raffaele.decarli@thread.solutions>
2020-12-17 16:47:44 +0100

    fix slider

--- js/main.js
+++ js/main.js
@@ -7,7 +7,13 @@
             scrollbar: {
                 draggable: false,
             },
-            navigation: false
+            on:{
+                slideChange: function (swiper){
+                    if(swiper.activeIndex === 1){
+                        $('.btn-prev.swiper-button-disabled').on('click', gotostart)
+                    }
+                }
+            }
         });
 
         var internal = new Swiper('.swiper-container-inn', {
@@ -18,27 +24,40 @@
                 nextEl: '.btn-next',
                 prevEl: '.btn-prev',
             },
+            on: {
+                slideChange: function (swiper){
+                    if(swiper.activeIndex === 0){
+                        $('.btn-prev.swiper-button-disabled').on('click', gotostart)
+                    }
+                    if(swiper.activeIndex === swiper.slides.length - 1){
+                        $('.btn-next.swiper-button-disabled').on('click', gotoend)
+                    }
+                    if(swiper.activeIndex > 0 && swiper.activeIndex < swiper.slides.length - 1){
+                        $('.btn-prev').off('click', gotostart)
+                        $('.btn-next').off('click', gotoend)
+                    }
+                }
+            }
         });
 
-        $('.ext-next-slide').click(function (e){
+
+        $('.ext-next-slide').click(function (e) {
             e.preventDefault();
             external.slideTo(1)
         })
-        $('.ext-start-slide').click(function (e){
+
+        $('.ext-start-slide').click(function (e) {
             e.preventDefault();
             external.slideTo(0)
             internal.slideTo(0)
         })
 
-        $(document).on('click','.btn-prev.swiper-button-disabled', function (e){
-            e.preventDefault();
+        var gotostart = function (){
             external.slideTo(0)
-        })
-
-        $(document).on('click','.btn-next.swiper-button-disabled', function (e){
-            e.preventDefault();
+        }
+        var gotoend = function () {
             external.slideTo(2)
-        })
+        }
 
     });
 })(jQuery);
